5dc58f763772bec3be3ba1265a83cf4336873ae0
Kathleen Jung <ovo0325@outlook.kr>
2022-11-01 15:47:10 +0900

    error 처리 중
    Former-commit-id: c3dd9976d28883ddfbc6c0ec28bc8a1135994fc9

---
+++ src/main/java/com/ssafy/board/controller/ExceptionController.java
@@ -0,0 +1,29 @@
+package com.ssafy.board.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ExceptionController implements ErrorController {
+
+    private static final String PATH = "/error"; // configure 에서 Redirect 될 path
+
+    @RequestMapping(value = PATH)
+    public String error(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (String.valueOf(status).equalsIgnoreCase(HttpStatus.NOT_FOUND.toString())) {
+            return "error/error"; // /WEB-INF/errors/404.jsp
+        }
+        return "error";
+    }
+
+    public String getErrorPath() {
+        return PATH;
+    }
+}
