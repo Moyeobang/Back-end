37acbf510f4e0a18b08ddda59bde6626b28b7633
홍예진 <teepij@naver.com>
2022-11-24 17:15:07 +0900

    add erd, edit sql
    Former-commit-id: c9eff3c35908186475f7785269387a4194fbc45a

--- src/main/resources/sql/Store_CSV_LOAD_DATA.sql
+++ src/main/resources/sql/Store_CSV_LOAD_DATA.sql
@@ -5,3 +5,6 @@ FIELDS TERMINATED by ","
 LINES TERMINATED BY "\n"
 IGNORE 1 ROWS
 (storeId, storeName, category, dongCode, address, longitude, latitude);
+
+delete from storeinfo where latitude < -90;
+delete from storeinfo where longitude < -180;
